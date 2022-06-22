79805837e06bdb81fcdb25a55193897da20491ac
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-06-22 09:56:52 +0100

    v4

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './article.css';
-const Article = () => {
+const Article = ({imageUrl,date,title} ) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imageUrl} alt='blog' />
+      </div>
+      <div>
+        <p>{date}</p>
+        <h3> {title} </h3>
+      </div>
+      <p>Read Full Article</p>
+    </div>
   )
 }
 
