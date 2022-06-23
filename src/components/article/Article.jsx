b4b01b05797fbac4602b9762f298a22072f7950c
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-06-23 11:07:02 +0100

    vf

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -4,15 +4,16 @@ const Article = ({imageUrl,date,title} ) => {
   return (
     <div className='gpt3__blog-container_article'>
       <div className='gpt3__blog-container_article-image'>
-        <img src={imageUrl} alt='blog' />
+        <img src={imageUrl} alt='blog_image' />
       </div>
+      <div className="gpt3__blog-container_article-content">
       <div>
         <p>{date}</p>
-        <h3> {title} </h3>
+        <h3>{title}</h3>
       </div>
       <p>Read Full Article</p>
     </div>
-  )
-}
+  </div>
+)};
 
-export default Article
+export default Article;
